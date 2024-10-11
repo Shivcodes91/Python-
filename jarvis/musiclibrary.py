ab4315fec1b26d50d1f9ed45e67aa106a027dd9f
Shivcodes91 <shivangrai238@gmail.com>
2024-10-11 17:20:32 +0530

    mega project

--- jarvis/musiclibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "stealth": "https://youtu.be/pfdhmoMIHv4?si=P8PbqWc8O92MoZXK",
-    "local": "https://youtu.be/sFMRqxCexDk?si=7UaXas8PTzhqt_fl",
-    "Million":"https://youtu.be/XO8wew38VM8?si=xqSaTq9KDFGWBGMi",
-    "shape":"https://youtu.be/JGwWNGJdvx8?si=1k0k_hw_bIJ0mZdG"
-} 
